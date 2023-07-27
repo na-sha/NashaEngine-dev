944a5db1fdd4d9e3cf84476d6f8ca3fb4c9e0d26
Nandan Sharma <69229070+na-sha@users.noreply.github.com>
2023-07-27 19:23:23 +0530

    Index Buffers

--- Nasha/src/Nasha/Model.cpp
+++ Nasha/src/Nasha/Model.cpp
@@ -4,13 +4,19 @@
 
 namespace Nasha{
 
-    Model::Model(VkSetup& device, const std::vector<Vertex> &vertices) : m_device{device}{
-        createVertexBuffers(vertices);
+    Model::Model(VkSetup& device, const Model::Builder& builder) : m_device{device}{
+        createVertexBuffers(builder.vertices);
+        createIndexBuffers(builder.indices);
     }
 
     Model::~Model(){
         vkDestroyBuffer(m_device.device(), m_vertexBuffer, nullptr);
         vkFreeMemory(m_device.device(), m_vertexBufferMemory, nullptr);
+
+        if (hasIndexBuffer) {
+            vkDestroyBuffer(m_device.device(), m_indexBuffer, nullptr);
+            vkFreeMemory(m_device.device(), m_indexBufferMemory, nullptr);
+        }
     }
 
     void Model::createVertexBuffers(const std::vector<Vertex> &vertices) {
@@ -18,30 +24,87 @@ namespace Nasha{
         assert(m_vertexCount >=3 && "Vertex Count must be greater or equal to 3");
         VkDeviceSize bufferSize = sizeof(vertices[0]) * m_vertexCount;
 
+        VkBuffer stagingBuffer{};
+        VkDeviceMemory stagingBufferMemory{};
+
         m_device.createBuffer(bufferSize,
-                              VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+                              VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
                               VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
-                              m_vertexBuffer,
-                              m_vertexBufferMemory);
+                              stagingBuffer,
+                              stagingBufferMemory);
 
         void* data;
         vkMapMemory(m_device.device(),
-                    m_vertexBufferMemory,
+                    stagingBufferMemory,
                     0,
                     bufferSize,
                     0,
                     &data);
         memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
+        vkUnmapMemory(m_device.device(), stagingBufferMemory);
+
+        m_device.createBuffer(bufferSize,
+                              VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+                              VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+                              m_vertexBuffer,
+                              m_vertexBufferMemory);
+
+        m_device.copyBuffer(stagingBuffer, m_vertexBuffer, bufferSize);
+        vkDestroyBuffer(m_device.device(), stagingBuffer, nullptr);
+        vkFreeMemory(m_device.device(), stagingBufferMemory, nullptr);
+    }
+
+    void Model::createIndexBuffers(const std::vector<uint32_t> &indices) {
+        m_indexCount = static_cast<uint32_t>(indices.size());
+        hasIndexBuffer = m_indexCount > 0;
+        if (!hasIndexBuffer) {
+            return;
+        }
+        VkDeviceSize bufferSize = sizeof(indices[0]) * m_indexCount;
+
+        VkBuffer stagingBuffer{};
+        VkDeviceMemory stagingBufferMemory{};
+
+        m_device.createBuffer(bufferSize,
+                              VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+                              VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+                              stagingBuffer,
+                              stagingBufferMemory);
+
+        void* data;
+        vkMapMemory(m_device.device(),stagingBufferMemory,0,bufferSize,0,&data);
+        memcpy(data, indices.data(), static_cast<size_t>(bufferSize));
+        vkUnmapMemory(m_device.device(), stagingBufferMemory);
+
+        m_device.createBuffer(bufferSize,
+                              VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+                              VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+                              m_indexBuffer,
+                              m_indexBufferMemory);
+
+        m_device.copyBuffer(stagingBuffer, m_indexBuffer, bufferSize);
+        vkDestroyBuffer(m_device.device(), stagingBuffer, nullptr);
+        vkFreeMemory(m_device.device(), stagingBufferMemory, nullptr);
     }
 
     void Model::bind(VkCommandBuffer commandBuffer) {
         VkBuffer buffers[] = { m_vertexBuffer };
         VkDeviceSize offsets[] = { 0 };
         vkCmdBindVertexBuffers(commandBuffer, 0, 1, buffers, offsets);
+
+        if (hasIndexBuffer){
+            vkCmdBindIndexBuffer(commandBuffer, m_indexBuffer, 0, VK_INDEX_TYPE_UINT32);
+        }
     }
 
     void Model::draw(VkCommandBuffer commandBuffer) {
-        vkCmdDraw(commandBuffer, m_vertexCount, 1, 0, 0);
+        if (hasIndexBuffer){
+            vkCmdDrawIndexed(commandBuffer,
+                             m_indexCount,
+                             1, 0, 0, 0);
+        }else {
+            vkCmdDraw(commandBuffer, m_vertexCount, 1, 0, 0);
+        }
     }
 
     std::vector<VkVertexInputBindingDescription> Model::Vertex::getBindingDescriptions() {
--- Nasha/src/Nasha/Model.h
+++ Nasha/src/Nasha/Model.h
@@ -16,7 +16,12 @@ namespace Nasha{
             static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
             static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
         };
-        Model(VkSetup& device, const std::vector<Vertex> &vertices);
+        struct Builder{
+            std::vector<Vertex> vertices{};
+            std::vector<uint32_t> indices{};
+        };
+
+        Model(VkSetup& device, const Model::Builder& builder);
         ~Model();
 
         Model(const Model&) = delete;
@@ -26,11 +31,18 @@ namespace Nasha{
         void draw(VkCommandBuffer commandBuffer);
     private:
         void createVertexBuffers(const std::vector<Vertex> &vertices);
+        void createIndexBuffers(const std::vector<uint32_t> &indices);
     public:
     private:
         VkSetup& m_device;
+
         VkBuffer m_vertexBuffer;
         VkDeviceMemory m_vertexBufferMemory;
         uint32_t m_vertexCount;
+
+        bool hasIndexBuffer{false};
+        VkBuffer m_indexBuffer;
+        VkDeviceMemory m_indexBufferMemory;
+        uint32_t m_indexCount;
     };
 }
